5da58c2902f1d669b445b8efb3b1ec66e2082815
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-20 06:46:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,4 +22,23 @@ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil pai
   shipping_manifest["oil paintings"]
 end
 
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
 
+
+
+  # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
+
+
+
+  # return the shipping_manifest hash below
+  shipping_manifest
+
+end
